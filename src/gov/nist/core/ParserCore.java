b234c21c1a184ad4cb554475a7e2d4bf99dff4e8
Matthieu Casanova <kpouer@users.sourceforge.net>
2022-12-31 11:15:01 +0100

    use AtomicInteger in ParserCore

--- src/gov/nist/core/ParserCore.java
+++ src/gov/nist/core/ParserCore.java
@@ -26,6 +26,7 @@
 package gov.nist.core;
 
 import java.text.ParseException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /** Generic parser class.
 * All parsers inherit this class.
@@ -40,7 +41,7 @@ import java.text.ParseException;
 public abstract class ParserCore {
     public static final boolean debug = Debug.parserDebug;
 
-    static int nesting_level;
+    static AtomicInteger nesting_level =new AtomicInteger();
 
     protected LexerCore lexer;
 
@@ -112,7 +113,7 @@ public abstract class ParserCore {
 
     protected  void dbg_enter(String rule) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < nesting_level ; i++)
+        for (int i = 0; i < nesting_level.get() ; i++)
             stringBuilder.append(">");
 
         if (debug)  {
@@ -121,12 +122,12 @@ public abstract class ParserCore {
                 "\nlexer buffer = \n" +
                 lexer.getRest());
         }
-        nesting_level++;
+        nesting_level.incrementAndGet();
     }
 
     protected void dbg_leave(String rule) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < nesting_level ; i++)
+        for (int i = 0; i < nesting_level.get() ; i++)
             stringBuilder.append("<");
 
         if (debug)  {
@@ -136,7 +137,7 @@ public abstract class ParserCore {
                 "\nlexer buffer = \n" +
                 lexer.getRest());
         }
-        nesting_level --;
+        nesting_level.decrementAndGet();
     }
 
     protected NameValue nameValue() throws ParseException  {
